d0ea536321cbf25a33f2f4199c2e057c57dc8c4c
sanjoyk <sanjoyksardar01@gmail.com>
2019-03-18 01:54:54 +0530

    make compilation in watch mode

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Home = ()=>{
-    return <div>I'm home page!</div>
+    return (
+        <div>
+            <div>I'm very first home page!</div>
+            <button style={{padding:"8px", fontweight:"bold", fontSize:"14px"}} conClick={()=> console.log("clicked!!")}>Click me!</button>
+        </div>
+        
+    )
 }
 export default Home;
